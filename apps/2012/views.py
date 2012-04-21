93243eb2c0953d9a1b2ede731f65acdb32998551
Daniel Lv <lgn21st@gmail.com>
2012-04-22 07:50:55 +0800

    Contents update for PMConf 2012

--- apps/2012/views.py
+++ apps/2012/views.py
@@ -10,19 +10,19 @@ def index():
 
 @expose('/2012')
 class siteView(object):
-        
+
     def about(self):
-        return dict(page=dict(pagename='about',cndata='')) 
-    
+        return dict(page=dict(pagename='about',cndata=''))
+
     def schedule(self):
         return dict(page=dict(pagename='schedule',cndata=''))
-    
+
     def venue(self):
-        return dict(page=dict(pagename='venue',cndata=''))    
+        return dict(page=dict(pagename='venue',cndata=''))
 
     def registration(self):
         return dict(page=dict(pagename='registration',cndata=''))
-        
+
     def sponsors(self):
         return dict(page=dict(pagename='sponsors',cndata=''))
 
